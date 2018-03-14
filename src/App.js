ce165285ffef7c15357a97ec77cf4b05293c7837
Gunjan Karun <gunjan@kalptaru.in>
2018-03-14 11:21:29 +0530

    Am able to download page and get header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Preview from './components/Preview';
 import ShareButtons from './components/ShareButtons';
 import UseIt from './components/UseIt';
 import About from './components/About';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -27,16 +28,59 @@ class App extends Component {
   }
 
   fetchExternalHTML(url) {
-    fetch(url, { mode: 'no-cors' }) // Call the fetch function passing the url of the API as a parameter
-      .then(function(result) {
-        // Your code for handling the data you get from the API
-        console.log('Loaded page ', result);
+    /*
+    <meta property="og:url" content="https://github.com">
+    <meta property="og:site_name" content="GitHub">
+    <meta property="og:title" content="Build software better, together">
+    <meta property="og:description" content="GitHub is where people build software. More than 27 million people use GitHub to discover, fork, and contribute to over 80 million projects.">
+    <meta property="og:image" content="https://assets-cdn.github.com/images/modules/open_graph/github-logo.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="1200">
+    <meta property="og:image" content="https://assets-cdn.github.com/images/modules/open_graph/github-mark.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="620">
+    <meta property="og:image" content="https://assets-cdn.github.com/images/modules/open_graph/github-octocat.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="620">
+    */
+
+    let website_object = {url: url};
+    const new_url = 'https://cors-anywhere.herokuapp.com/' + url;
+    axios
+      .get(new_url)
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(data, 'text/html');
+        const titles = htmlDoc.getElementsByTagName('title'); 
+        website_object.title = titles[0].innerHTML;
+        console.log('website_object is ', website_object);
+
+        var meta_tags = document.getElementsByTagName('meta');
+        console.log(meta_tags);
+
       })
-      .catch(function(error) {
+      .catch(error => {
         // This is where you run code if the server returns any errors
         console.log('Got fetch error ', error);
       });
   }
+
+  // fetchExternalHTML(url) {
+  //   fetch(url, { mode: 'no-cors' }) // Call the fetch function passing the url of the API as a parameter
+  //     .then(function(result) {
+  //       // Your code for handling the data you get from the API
+  //       console.log('Loaded page ', result);
+  //     })
+  //     .catch(function(error) {
+  //       // This is where you run code if the server returns any errors
+  //       console.log('Got fetch error ', error);
+  //     });
+  // }
 
   componentWillMount() {
     const ref_url = document.referrer;
